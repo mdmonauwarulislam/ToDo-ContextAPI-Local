d89f4cb22cf23ce650fc330a6982c36161993cd8
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-03-01 02:44:12 +0530

    rrd

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
     <App />
-    </HashRouter>HashRouter
+    </HashRouter>
   </React.StrictMode>,
 )
